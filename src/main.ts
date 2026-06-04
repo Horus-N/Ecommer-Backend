f2b19041501b20d01c8274d41aa4be099b4263ab
T_dev Nguyen <tungkim614@gmail.com>
2026-06-04 09:27:29 +0700

    setup prisma for project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Đặt prefix cho toàn bộ API (Ví dụ: http://localhost:3000/api/v1/...)
+  app.setGlobalPrefix('api');
+  // Kích hoạt tính năng shutdown hooks để Prisma đóng kết nối an toàn khi tắt server
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
